a3500b7328179fcaf6a1e80e98060a1266c21a99
Lucien Bénié <lucien.benie@logmein.com>
2019-10-11 16:11:55 -0400

    chore(eslint): add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'prettier/prettier': ['error', { singleQuote: true }],
+  },
+}
